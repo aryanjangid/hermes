5585dca44e940db1060fbec9ac40462c60f1e8c8
Sarthak Pawar <sarthakrajesh777@gmail.com>
2022-09-24 13:14:17 +0530

    buy page done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Route, Routes, useNavigate } from 'react-router';
 
 const Home = () => {
+  const navigate = useNavigate()
   return (
     <div>
-      
+      <button onClick={() => navigate('/buy')}>buy</button>
+      <button onClick={() => navigate('/sell')}>sell</button>
     </div>
   )
 }
