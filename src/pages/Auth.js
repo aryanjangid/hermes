711b29ec417c6e38c4b1dd15e28edcc1c49d1a50
Palash Rambhia <palashrambhia2@gmail.com>
2022-09-24 14:07:40 +0530

    Sign up done

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,12 +1,12 @@
 import React from 'react'
 // import Login from '../Components/Login'
-// import Signup from '../Components/Signup'
+import Signup from '../components/Signup'
 
 const Auth = () => {
     return (
         <div>
             {/* <Login></Login> */}
-            {/* <Signup></Signup> */}
+            <Signup></Signup>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,6 @@
 import React from 'react'
-<<<<<<< HEAD
-=======
 import { Route, Routes, useNavigate } from 'react-router';
 import Mess from './Mess';
->>>>>>> 52a6c9fa48abdeb1efc371c89dfdd8693d759215
 
 const Home = () => {
   const navigate = useNavigate()
